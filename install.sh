9b323d1bab7adc282ecd76d5c57f6194d98d876f
Sahil Gulati <sahilgulati102@gmail.com>
2021-07-25 11:29:34 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ git clone --recursive https://github.com/ActivityWatch/activitywatch.git
 cp activityWatcher/Dockerfile ../activitywatch/
 cd activitywatch
 docker build -t activitywatch .
-docker run -it activitywatch make clean package/build
+#docker run -it activitywatch make clean package/build ## build for Dev env and clean pacakge for Prod and Test
+## Since there are some compilation issues with the repo hence using dummy to generate container id
+docker run -it activitywatch bash ## Comment this if compilation issues are fixed
 containerid=$(docker ps -all | grep activitywatcher | head -n 1 | awk '{print $1}')
 docker cp $containerid:/opt/activitywatch .
